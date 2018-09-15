478095f151bc236fe3b2bdb7959f10133f5fb166
Nigel Gormley <nigel.gormley@gmail.com>
2018-09-15 23:40:17 +0100

    End of Lecture 41 - adding search results

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,39 @@
-// Global app controller
-import x from './test';
+import  Search from './models/Search';
+import * as searchView from './views/searchView'
+import {elements} from './views/base';
 
-const y = 23;
-console.log(`I imported ${x} from another module! and ${y}`);
+// Global state of the App
+//
+// -- Search Object
+// -- Current recipe object
+// -- Shopping list object
+// -- Liked Recipes
+//
+
+const state = {
+}
+
+const controllerSearch = async () => {
+    
+    const query = searchView.getInput();
+
+    if (query) {
+        state.search = new Search(query);
+
+        // 3 Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        await state.search.getResults();
+
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controllerSearch();
+});
+
+
+
