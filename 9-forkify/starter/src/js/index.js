ccb3538a39077abc6096f2cd86e9ef5c26b0afd6
Nigel Gormley <nigel.gormley@gmail.com>
2018-09-19 22:47:44 +0100

    End of Lecture 147 - recipe Model built.

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -21,15 +21,20 @@ const controllerSearch = async () => {
     if (query) {
         state.search = new Search(query);
 
-        // 3 Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
         renderLoader(elements.searchResults);
         
-        await state.search.getResults();
+        try {
+            await state.search.getResults();
 
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (error) {
+            alert('Something went wrong with search');
+            clearLoader;
+        }
+
     }
 }
 
@@ -49,7 +54,30 @@ elements.searchResultPages.addEventListener('click', e=> {
 
 // Recipe Controller
 
-const r = new Recipe(46956);
-r.getRecipe();
-// console.log(r);
+const controlRecipe = async () => {
+    const id = window.location.hash.replace('#','');
+    
+    if (id) {
+        
+        // Create New Recipe Object
+        state.recipe = new Recipe(id);
+        
 
+        try {
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+            state.recipe.calcServings();
+            state.recipe.calcTime();
+
+            console.log(state.recipe);
+        } catch (error) {
+            alert('Error Loading Recipe');
+        }
+
+        
+    }
+};
+
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
--- 9-forkify/starter/src/js/models/Recipe.js
+++ 9-forkify/starter/src/js/models/Recipe.js
@@ -8,17 +8,76 @@ export default class Recipe {
 
     async getRecipe() {
         try {
-            const result = await axios(`${proxy}http://food2fork.com/api/get?key=${key}&rId=${this.id}`);
-            console.log(result);
-            
-            // this.title = result.data.recipe.title;
-            // this.author = result.data.recipe.publisher;
-            // this.img = result.data.recipe.img_url;
-            // this.url = result.data.recipe.source_url;
-            // this.ingredients = result.data.recipe.ingredients;
+            const result = await axios(`http://food2fork.com/api/get?key=${key}&rId=${this.id}`);
+
+            this.title = result.data.recipe.title;
+            this.author = result.data.recipe.publisher;
+            this.img = result.data.recipe.image_url;
+            this.url = result.data.recipe.source_url;
+            this.ingredients = result.data.recipe.ingredients;
         } catch (error) {
             console.log(error);
-            
+            alert('Something went wrong :(');
         }
+    }
+
+    calcTime() {
+        const numIng = this.ingredients.length;
+        const period = Math.ceil(numIng/3);
+        this.time = period * 15;
+    }
+
+    calcServings() {
+        this.servings = 4;
+    }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon','ounces', 'ounce', 'teaspoons', 'teaspoon','cups', 'pounds'];
+        const unitsShort = ['tbsp','tbsp', 'oz', 'oz', 'tsp','tsp','cup','pound'];
+
+        const newIngredients = this.ingredients.map(el =>{
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit,i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+
+            ingredient = ingredient.replace(/ *\([^)]*\) */g,' ');
+
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+
+            let objIng;
+            if (unitIndex > -1) {
+                const arrCount = arrIng.slice(0, unitIndex);
+                let count;
+
+                if (arrCount.length===1) {
+                    count = eval(arrIng[0].replace('-','+'));
+                } else {
+                    count = eval(arrIng.slice(0,unitIndex).join('+'));
+                }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex +1).join(' '),
+                }
+            } else if (parseInt(arrIng[0], 10)) {
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                }
+            } else if (unitIndex === -1) {  
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                }
+            }
+
+            return objIng;
+        });
+        this.ingredients = newIngredients;
     }
 }
