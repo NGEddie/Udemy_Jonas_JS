825ca5c558a67b948518ed3aa06b567e364cc273
Nigel Gormley <nigel.gormley@gmail.com>
2018-09-23 13:15:23 +0100

    can't rember - added buttons that work

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,8 +1,13 @@
 import  Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
+
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
+import * as listView from './views/listView'
 import {elements, renderLoader, clearLoader} from './views/base';
+
+
 
 // Global state of the App
 //
@@ -12,8 +17,14 @@ import {elements, renderLoader, clearLoader} from './views/base';
 // -- Liked Recipes
 //
 
-const state = {
-}
+const state = {};
+window.state = state;
+
+
+// *********************
+// * SEARCH CONTROLLER *
+// *********************
+
 
 const controllerSearch = async () => {
     
@@ -53,7 +64,9 @@ elements.searchResultPages.addEventListener('click', e=> {
     }    
 });
 
-// Recipe Controller
+// *********************
+// * RECIPE CONTROLLER *
+// *********************
 
 const controlRecipe = async () => {
     const id = window.location.hash.replace('#','');
@@ -87,6 +100,50 @@ const controlRecipe = async () => {
         
     }
 };
+// *******************
+// * LIST CONTROLLER *
+// *******************
 
+const controlList = () => {
+    // Create a new list if there is none yet
+    if (!state.list) { state.list = new List(); }
+
+    // Add each ingredient to the list
+    state.recipe.ingredients.forEach(ingredient => {
+        const item = state.list.addItem(ingredient.count, ingredient.unit, ingredient.ingredient);
+        listView.renderItem(item);
+    });
+}
+//Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        //Delete from state
+        state.list.deleteItem(id);
+        //delete from UI
+        listView.delItem(id);
+    }
+});
+
+// *******************
+// * EVENT LISTENERS *
+// *******************
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.clearRecipe();
+            recipeView.renderRecipe(state.recipe)
+        }
+    }else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        state.recipe.updateServings('inc');
+        recipeView.clearRecipe();
+        recipeView.renderRecipe(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {        
+        controlList();
+    }
+});
+
--- 9-forkify/starter/src/js/views/recipeView.js
+++ 9-forkify/starter/src/js/views/recipeView.js
@@ -62,12 +62,12 @@ export const renderRecipe = recipe => {
                     <span class="recipe__info-text"> servings</span>
 
                     <div class="recipe__info-buttons">
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-decrease">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-minus"></use>
                             </svg>
                         </button>
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-increase">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-plus"></use>
                             </svg>
@@ -87,7 +87,7 @@ export const renderRecipe = recipe => {
                     ${recipe.ingredients.map(el => createIngredient(el)).join('')}
                 </ul>
 
-                <button class="btn-small recipe__btn">
+                <button class="btn-small recipe__btn recipe__btn--add">
                     <svg class="search__icon">
                         <use href="img/icons.svg#icon-shopping-cart"></use>
                     </svg>
